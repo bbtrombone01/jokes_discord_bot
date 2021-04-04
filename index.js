893e86a4bb67f0a96e3806d50e96deb2ee6424a7
bbtrombone <bbtrombone@sbcglobal.net>
2021-04-04 05:56:01 -0700

    added logic to index.js

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
+// has discord bot tokem 
 require("dotenv").config()
-
-const arrayOfJokes = require("./jokesList")
-// console.log(process.env.discord_bot_token)
 
 const {Client} = require('discord.js')
 const discord_client = new Client
 
+// array of all the jokes for bot
+const arrayOfJokes = require("./jokesList")
+
+// look into find out what the prefix for other channels are 
+const prefix = "!"
 
 discord_client.on(`ready`, ()=>{
-    message.send("time to tell some jokes")
+    console.log("time to tell some jokes")
 })
 
 discord_client.on(`message`,(message) =>{
     if (message.author.bot) return
-    message.channel.send("why do i exist")
-    console.log(arrayOfJokes.arrayOfJokes.length)
-    setTimeout(() =>{
-        message.channel.send(" please tell me")
-    }, 30000)
+    if (message.content.startsWith(prefix)){
+        let indexOfJokeArray = parseInt(message.content.slice(prefix.length))
+        if (message.content.slice(prefix.length) === "random joke"){
+            console.log("set up this code later")
+        }
+        if( indexOfJokeArray < 50){
+            console.log("progress")
+            message.channel.send(arrayOfJokes.arrayOfJokes[indexOfJokeArray-1][0])
+            setTimeout(()=>{
+                message.channel.send(arrayOfJokes.arrayOfJokes[indexOfJokeArray-1][1])
+            },30000)
+        }
+    }
 })
 
 discord_client.login(process.env.discord_bot_token)
