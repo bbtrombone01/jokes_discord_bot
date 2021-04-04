bec9c76c457a2730ea6031a9c89e0eac14133e15
bbtrombone <bbtrombone@sbcglobal.net>
2021-04-04 02:16:41 -0700

    added jokesList.js

---
+++ index.js
@@ -0,0 +1,23 @@
+require("dotenv").config()
+
+const arrayOfJokes = require("./jokesList")
+// console.log(process.env.discord_bot_token)
+
+const {Client} = require('discord.js')
+const discord_client = new Client
+
+
+discord_client.on(`ready`, ()=>{
+    console.log("time to tell some jokes")
+})
+
+discord_client.on(`message`,(message) =>{
+    if (message.author.bot) return
+    message.channel.send("why do i exist")
+    console.log(arrayOfJokes.arrayOfJokes.length)
+    setTimeout(() =>{
+        message.channel.send(" please tell me")
+    }, 30000)
+})
+
+discord_client.login(process.env.discord_bot_token)
